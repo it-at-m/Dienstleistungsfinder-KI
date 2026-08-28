706bf496f00eda6078683a94a87f95f991e4b17e
Sebastian.Berger <sebastian.berger@muenchen.de>
2026-08-28 10:30:24 +0200

    run prettier

--- core/backend/static/loader.js
+++ core/backend/static/loader.js
@@ -1,1 +1,1 @@
-import(document.currentScript ? new URL('./src/entry-dlf-search-webcomponent-BapoaqfX.js', document.currentScript.src).href : './src/entry-dlf-search-webcomponent-BapoaqfX.js');
+import(document.currentScript ? new URL('./src/entry-dlf-search-webcomponent-CvWVcQdE.js', document.currentScript.src).href : './src/entry-dlf-search-webcomponent-CvWVcQdE.js');
